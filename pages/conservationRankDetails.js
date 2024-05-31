830b4c3ecb126bd59616a7573108b3cbab6b4610
plantsnb <plantsnb@gmail.com>
2024-05-31 11:21:34 -0300

    Mod Broswer Tab Labels for Details Pages

--- pages/conservationRankDetails.js
+++ pages/conservationRankDetails.js
@@ -2,12 +2,20 @@ import Footer from "../components/layouts/Footer";
 import Navbar from "../components/layouts/Navbar";
 import ConservationRankDetails from "../components/conservation-rank/conservationRankDetails";
 import Head from "next/head";
+import { useRouter} from "next/router";
 
 const ConservationRankDetailsPage = () => {
+  const router = useRouter()
   return (
     <>
       <Head>
-        <title>Conservation Rank Details | New Brunswick Plants</title>
+        <title>{router.query.keyword === 'Invasive'? 'Invasive Species' : 
+        router.query.keyword === 's1: critically imperiled'? 'S1'
+        : router.query.keyword === 's2: imperiled'? 'S2'
+        : router.query.keyword === 's3: vulnerable'? 'S3'
+        : router.query.keyword === 's4: apparently secure'? 'S4'
+        : router.query.keyword === 's5: secure'? 'S5'
+        : 'SNA'} | NB Plants</title>
       </Head>
       <Navbar />
       <ConservationRankDetails />
