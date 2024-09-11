bb8e218bce44ca91b69fe6c6e815aa044fed9d6a
plantsnb <plantsnb@gmail.com>
2024-09-11 08:31:52 -0300

    Removed Login Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import "bootstrap-icons/font/bootstrap-icons.css"
 import AuthComponent from "../components/front/AuthComponent"
+import Navbar from "../components/layouts/Navbar";
 import styles from "../styles/index.module.css"
 import Head from "next/head"
+import Headline from "../components/overview/Headline"
+import SpeciesOption from "../components/overview/SpeciesOption"
+import Footer from "../components/layouts/Footer";
 
 export default function Index() {
   return (
@@ -9,7 +13,7 @@ export default function Index() {
       <Head>
         <title>Home | NB Plants</title>
       </Head>
-      <div className={styles.selectionArea}>
+      {/* <div className={styles.selectionArea}>
         <div className="row justify-content-center">
           <div className="col-lg-8">
             <div className="d-flex flex-column text-center">
@@ -17,7 +21,15 @@ export default function Index() {
             </div>
           </div>
         </div>
+      </div> */}
+      <Navbar />
+      <div className="col-md-12 col-lg-12 " >
+        <div className="d-flex flex-column text-center">
+          <Headline />
+          <SpeciesOption />
+        </div>
       </div>
+      <Footer isFixed={true} />
     </div>
   )
 }
